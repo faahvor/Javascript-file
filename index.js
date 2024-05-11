148eeccb4b0a444be0683cb22bcd42e2ef92209e
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-05-11 20:52:20 +0100

    visual

---
+++ index.js
@@ -0,0 +1,16 @@
+function decodeColor(code){
+
+    switch(code){
+        case 1:
+            console.log("blue")
+            break;
+        case 2:
+            console.log("red");
+            break;
+        case 3:
+            console.log("pink");
+            break;
+        default:
+            console.log("no color");
+    }
+}
